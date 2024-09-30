ae9acc22a2dd9d7849ebbd572354ce9448046cb4
unknown <malondrasarian@gmail.com>
2024-09-30 13:31:19 +0800

    Update

--- resources/views/user/training-assessment/attendees/print-certificate.blade.php
+++ resources/views/user/training-assessment/attendees/print-certificate.blade.php
@@ -118,7 +118,7 @@
             <h1 style="font-family: 'Monotype-Corsiva' !important;" class="whitespace-nowrap text-[57px] absolute top-[382px] left-1/4 -translate-x-1/2">{{ ucwords(strtolower($attendee->name)) }}</h1>
             <h2 style="font-family: 'Perpetua'" class="text-lg absolute top-[445px] left-1/4 -translate-x-1/2 uppercase whitespace-nowrap">{{ $training->customer->name }}</h2>
             <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[548px] left-1/4 -translate-x-1/2 text-center leading-[26px] whitespace-nowrap">has attended the Comprehensive Training on <br> Basic Safety Operators Training <span class="font-bold">{{ $attendee->brand . ' ' . $attendee->type }}</span></h2>
-            <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[610px] left-1/4 -translate-x-1/2 text-center">Given on this {{!! $formattedDate !!}}</h2>
+            <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[610px] left-1/4 -translate-x-1/2 text-center">Given on this {!! $formattedDate !!}</h2>
             <img style="{{ $thSizeClass.$thTop }}" src="{{ asset('storage/'.$trainer_head->signature) }}" class="absolute left-[13.2954%] -translate-x-1/2">
             <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[772px] left-[13.2954%] -translate-x-1/2">{{ ucwords(strtolower($trainer_head->first_name . ' ' . $trainer_head->last_name)) }}</h2>
             <img style="{{ $tSizeClass.$tTop }}" src="{{ asset('storage/'.$training->trainerName->signature) }}" class="absolute left-[36.7045%] -translate-x-1/2">
@@ -129,7 +129,7 @@
                 <h1 style="font-family: 'Monotype-Corsiva' !important;" class="whitespace-nowrap text-[57px] absolute top-[382px] left-3/4 -translate-x-1/2">{{ ucwords(strtolower($attendee->name)) }}</h1>
                 <h2 style="font-family: 'Perpetua'" class="text-lg whitespace-nowrap absolute top-[445px] left-3/4 -translate-x-1/2 uppercase">{{ $training->customer->name }}</h2>
                 <h2 style="font-family: 'Perpetua'" class="text-xl whitespace-nowrap absolute top-[548px] left-3/4 -translate-x-1/2 text-center leading-[26px]">has succesfully completed the Comprehensive Training on <br> Basic Safety Operators Training <span class="font-bold">{{ $attendee->brand . ' ' . $attendee->type }}</span></h2>
-                <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[610px] left-3/4 -translate-x-1/2 text-center">Given on this {{!! $formattedDate !!}}</h2>
+                <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[610px] left-3/4 -translate-x-1/2 text-center">Given on this {!! $formattedDate !!}</h2>
                 <h2 style="font-family: 'Perpetua'" class="text-2xl absolute font-bold top-[637px] left-3/4 -translate-x-1/2 uppercase">Class {{$class}} ({{ $overallScore }}%) Level {{$attendee->level}}</h2>
                 <img style="{{ $thSizeClass.$thTop }}" src="{{ asset('storage/'.$trainer_head->signature) }}" class="absolute left-[63.2954%] -translate-x-1/2">
                 <h2 style="font-family: 'Perpetua'" class="text-xl absolute top-[772px] left-[63.2954%] -translate-x-1/2">{{ ucwords(strtolower($trainer_head->first_name . ' ' . $trainer_head->last_name)) }}</h2>
